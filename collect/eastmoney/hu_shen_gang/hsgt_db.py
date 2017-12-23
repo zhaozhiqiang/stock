11b97b7b2101df8361f7141b5af5a648dd724b45
zhaozhiqiang <iezhaozhiqiang@gmail.com>
2017-12-24 01:39:34 +0800

    save data to sqlite

---
+++ collect/eastmoney/hu_shen_gang/hsgt_db.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+
+HSGT_DB = '../../../data/hsgt.db'
+HSGT_COLUMNS = '''(date, s_code, s_name, close_price, up_percentage, share_hold_sum, share_rate, share_hold_price, share_hold_price_1, share_hold_price_5, share_hold_price_10)'''
+
+
+class HSGTDB:
+    conn = sqlite3.connect(HSGT_DB)
+    cursor = conn.cursor()
+
+    def __init__(self):
+        self.create_hsgt()
+
+    def create_hsgt(self):
+        self.cursor.execute(
+            '''CREATE TABLE IF NOT EXISTS hsgt ''' +
+            HSGT_COLUMNS)
+
+    def __del__(self):
+        self.dispose()
+
+    def connect(self):
+        self.conn = sqlite3.connect(HSGT_DB)
+        self.cursor = self.conn.cursor()
+
+    def dispose(self):
+        self.cursor.close()
+        self.conn.commit()
+        self.conn.close()
+
+    def insert_data(self, data):
+        self.cursor.executemany(
+            "insert into hsgt  values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
+
+
+if '__main__' == __name__:
+    hsgt_db = HSGTDB()
+    hsgt_db.create_hsgt()
---
+++ collect/eastmoney/hu_shen_gang/store_to_sqlite.py
@@ -0,0 +1,85 @@
+import json
+import logging
+import os
+import sqlite3
+
+from tqdm import tqdm
+
+from hsgt_db import HSGTDB
+
+
+class SaveToSqlite:
+    HSG_INFO = 'hsg.json'
+
+    def __init__(self):
+        self.hsg_info = self.get_json()
+
+    def get_json(self):
+        if os.path.exists(self.HSG_INFO):
+            with open(self.HSG_INFO, 'r') as f:
+                return json.load(f)
+        else:
+            logging.error('Missed file: hsg.json')
+
+    def get_original_str(self, file_name):
+        if os.path.exists(self.HSG_INFO):
+            with open(file_name, 'r') as f:
+                return f.read()
+        else:
+            return ''
+
+    def get_useful_str(self, original_str):
+        useful_begin = self.hsg_info['hgt'][0]['useful_begin']
+        useful_end = self.hsg_info['hgt'][0]['useful_end']
+        return original_str[useful_begin:useful_end]
+
+    def get_item(self, data):
+        item = []
+        item.append(data['HDDATE'][:-9])
+        item.append(data['SCODE'])
+        item.append(data['SNAME'])
+        item.append(data['CLOSEPRICE'])
+        item.append(data['ZDF'])
+        item.append(data['SHAREHOLDPRICE'])
+        item.append(data['SHARESRATE'])
+        item.append(data['SHAREHOLDPRICE'])
+        item.append(data['SHAREHOLDPRICEONE'])
+        item.append(data['SHAREHOLDPRICEFIVE'])
+        item.append(data['SHAREHOLDPRICETEN'])
+        return item
+
+    def str_to_list(self, string):
+        datas = json.loads(string)
+        iterms = []
+        for data in datas:
+            iterms.append(self.get_item(data))
+        return iterms
+
+    def save_content_to_sqlite(self, content):
+        hsgt_db = HSGTDB()
+        hsgt_db.connect()
+        hsgt_db.insert_data(content)
+
+    def save_file_to_sqlite(self, file_name):
+        original_str = self.get_original_str(file_name)
+        useful_str = self.get_useful_str(original_str)
+        content = self.str_to_list(useful_str)
+        self.save_content_to_sqlite(content)
+
+    def process_all_files_in_folder(self, folder):
+        file_list = [
+            f for f in os.listdir(
+                folder) if '.txt' in f]
+        for f in tqdm(file_list):
+            file_name = os.path.join(folder, f)
+            self.save_file_to_sqlite(file_name)
+
+    def process_all_folders(self):
+        for hsg in self.hsg_info:
+            print(self.hsg_info[hsg][0]['name'])
+            self.process_all_files_in_folder(self.hsg_info[hsg][0]['folder'])
+
+
+if '__main__' == __name__:
+    sts = SaveToSqlite()
+    sts.process_all_folders()
