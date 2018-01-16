ed2f566d61774cefabc7e430ec90d8edf5d778ea
zhaozhiqiang <iezhaozhiqiang@gmail.com>
2018-01-17 00:30:34 +0800

    import logging

--- collect/eastmoney/hu_shen_gang/hsg.py
+++ collect/eastmoney/hu_shen_gang/hsg.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 
-from subcmds import all_commands
-from command_hsg import CommandHSG
+import logging
+import os
 import sys
+from logging import config
+
+import yaml
+
+from command_hsg import CommandHSG
+from subcmds import all_commands
+
+# load logging config
+CODE_PATH = os.path.dirname(os.path.abspath(__file__))
+with open(CODE_PATH + '/log/logging.yaml', 'r') as f:
+    config = yaml.safe_load(f.read())
+logging.config.dictConfig(config)
+
+# disable pyshark's log
+logger_trollius = logging.getLogger('trollius')
+logger_trollius.addHandler(logging.NullHandler())
+logger_trollius.propagate = False
 
 
 if '__main__' == __name__:
